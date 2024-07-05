859647e771788008e55c96433f5947550897f8ea
Dhruv Rajput <dhruvrajput2001@gmail.com>
2024-07-05 19:46:35 +0530

    updated auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,29 +3,32 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-const verifyJWT = asyncHandler(async(req, res, next) => {
-    try {
-        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", ""); // or part is for the mobile browser
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", ""); // or part is for the mobile browser
 
-        if(!token) {
-            throw new ApiError(401, "Unauthorized access");
-        }
-    
-        const decodedTokenInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    
-        const user = await User.findById(decodedTokenInfo._id).select("-password -refreshToken");
-    
-        if(!user) {
-            throw new ApiError(401, "Invalid access token");
-        }
-    
-        req.user = user; // this req.user will be used in logging out the user
-        next();
+    console.log("Token received:", token);
 
-    } catch (error) {
-        throw new ApiError(401, `error in verifying token: ${error.message}`);
+    if (!token) {
+      throw new ApiError(401, "Unauthorized access");
     }
 
-})
+    const decodedTokenInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log("Decoded token info:", decodedTokenInfo);
 
-export {verifyJWT};
+    const user = await User.findById(decodedTokenInfo._id).select("-password -refreshToken");
+    console.log("User found:", user);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user; // this req.user will be used in logging out the user
+    next();
+  } catch (error) {
+    console.error("Error in verifying token:", error);
+    throw new ApiError(401, `Error in verifying token: ${error.message}`);
+  }
+});
+
+export { verifyJWT };
