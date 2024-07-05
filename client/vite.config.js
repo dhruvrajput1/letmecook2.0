1541e2a1d8d14e5b0136f326367844d86a4117d0
Dhruv Rajput <dhruvrajput2001@gmail.com>
2024-07-05 17:06:33 +0530

    updated api proxy link

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://letmecook2-0.onrender.com'
     }
   }
 })
